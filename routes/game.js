059d8fed7e95020092dfb40e755cd61760182576
1627143 <1627143@stud.hs-mannheim.de>
2018-01-11 08:20:59 +0100

    eslint

--- routes/game.js
+++ routes/game.js
@@ -1,123 +1,100 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 module.exports= function(io){
-  const WATER = 0;
-  const SHIP = 1;
-  const MISSED_SHOOT = -1;
-  const HIT = 2;
+    const WATER = 0;
+    const SHIP = 1;
+    const MISSED_SHOOT = -1;
+    const HIT = 2;
+    const ROWS = 10;
+    const COLUMNS = 10;
 
-  let board1;
-  let board2;
-  let player1 = 'undefined';
-  let player2 = 'undefined';
-  let playerName1;
-  let playerName2;
-  let currentPlayer;
+    let board1;
+    let board2;
+    let player1 = "undefined";
+    let player2 = "undefined";
+    let playerName1;
+    let playerName2;
+    let currentPlayer;
 
-io.on('connection', function(socket){
+    io.on("connection", function(socket){
 
-  socket.on('setPlayerID', function(data){
-    if(player1 === 'undefined'){
-      player1 = data.id;
-      board1 = data.board;
-      console.log(board1);
-    }else if(player2 === 'undefined'){
-      player2 = data.id;
-      board2 = data.board;
-      console.log(board2);
-      io.sockets.emit('initalizeGameAndPlayer', {id1: player1, id2: player2, board1: board1, board2: board2});
-    }
+        socket.on("setPlayerID", function(data){
+            if(player1 === "undefined"){
+                player1 = data.id;
+                board1 = data.board;
+                console.log(board1);
+            }else if(player2 === "undefined"){
+                player2 = data.id;
+                board2 = data.board;
+                console.log(board2);
+                io.sockets.emit("initalizeGameAndPlayer", {id1: player1, id2: player2, board1: board1, board2: board2});
+            }
 
-  });
+        });
 
-  socket.on('setBoard', function(data){
-    console.log(data);
-    if(data.id == player1){
-      board2 = data.board;
-    }else if (data.id == player2){
-      board1 = data.board
-    }
-    io.sockets.emit('updateOwnBoard', {id1: player1, id2: player2, board1: board1, board2: board2});
-  });
+        socket.on("setBoard", function(data){
+            console.log(data);
+            if(data.id == player1){
+                board2 = data.board;
+            }else if (data.id == player2){
+                board1 = data.board;
+            }
+            io.sockets.emit("updateOwnBoard", {id1: player1, id2: player2, board1: board1, board2: board2});
 
-  socket.on('turnChange', function(data){
-    console.log('turnChange');
-    if(data.id == player1){
-      currentPlayer = player2;
-      console.log('Player2 turn');
-    }if(data.id == player2){
-      currentPlayer = player1;
-      console.log('Player1 turn');
-    }
-    io.sockets.emit('whosTurn', {id: currentPlayer});
-  });
-  socket.on('saveName', function(data){
-    if(data.id == player1){
-      playerName1 = data.name;
-    }else if(data.id == player2){
-      playerName2 = data.name;
-    }
-    if(playerName1 !== 'undefined' && playerName2 !== 'undefined'){
-      io.sockets.emit('opponentName', {name1: playerName1, name2: playerName2});
-    }
+            // Player 1 won
+            if(winner(board2)){
+                io.sockets.emit("winner",{id: player1});
+            }
+            // Player2 won
+            if(winner(board1)){
+                io.sockets.emit("winner",{id: player2});
+            }
 
-  });
-});
+        });
 
-function checkBoard(board, posX, posY){
-  if(board[posX][posY] == SHIP){
-    return true;
-  }else{
-    return false;
-  }
-}
+        socket.on("turnChange", function(data){
+            console.log("turnChange");
+            if(data.id == player1){
+                currentPlayer = player2;
+                console.log("Player2 turn");
+            }if(data.id == player2){
+                currentPlayer = player1;
+                console.log("Player1 turn");
+            }
+            io.sockets.emit("whosTurn", {id: currentPlayer});
+        });
+        socket.on("saveName", function(data){
+            if(data.id == player1){
+                playerName1 = data.name;
+            }else if(data.id == player2){
+                playerName2 = data.name;
+            }
+            if(playerName1 !== "undefined" && playerName2 !== "undefined"){
+                io.sockets.emit("opponentName", {name1: playerName1, name2: playerName2});
+            }
+        });
+    });
 
-    /*if(roundCounter%2 == 0){
-      socket.emit('currentPlayer', {id: player1});
-    }else{
-      socket.emit('currentPlayer', {id: player2});
-    }
-    socket.on('setBoard', function(board){
-      if(socketCounter % 2 != 0) {
-        board1 = {id: player1, board: board.board};
-        console.log("board1");
-        console.log(board1);
-      }if(socketCounter % 2 == 0){
-        board2 ={id: player2, board: board.board};
-        console.log("board2");
-        console.log(board2);
-      }
-      //console.log(board);
-        if(socketCounter % 2 != 0 && board2 !== 'undefined'){
-          socket.emit('getOpponentBoard', {board: board2});
+    function checkBoard(board, posX, posY){
+        if(board[posX][posY] == SHIP){
+            return true;
+        }else{
+            return false;
         }
-        if(socketCounter % 2 == 0 && board1 !== 'undefined'){
-          socket.emit('getOpponentBoard', {board: board1});
+    }
+
+    // check if all ships at the opponent Board are hitted
+    function winner(board){
+        for(let i = 0; i < COLUMNS; i++){
+            for(let j = 0; j < ROWS; j++){
+                if(board[i][j] == SHIP){
+                    return false;
+                }
+            }
         }
-
-  });*/
-
-
-  /*socket.on('shoot', function(data){
-  console.log(data);
-  if(data.id == player1){
-    if(board2[data.x][data.y] == SHIP){
-      socket.emit('shootResult', true);
-      console.log("Player1 " + true);
-    }else{
-      socket.emit('shootResult', false);
-      console.log("Player1 " + false);
+        return true;
     }
-  }else{
-    if(board1[data.x][data.y] == SHIP){
-      socket.emit('shootResult', true);
-      console.log("Player2 " +true);
-    }else{
-      socket.emit('shootResult', false);
-      console.log("Player2 " +false);
-    }
-  }
-});*/
-return router;
-}
+
+    return router;
+};
--- routes/highscore.js
+++ routes/highscore.js
@@ -1,51 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
-router.get('/', (req, res) => {
-  console.log("GET FUNCTION");
-	// Read Highscores
-	fs.readFile("files/highscore.json", "utf8", function(err, data){
-    console.log("read File");
-		if(err) {
-      console.log("Error");
-			res.status(500);
-			return;
-		}
+router.get("/", (req, res) => {
+    console.log("GET FUNCTION");
+    // Read Highscores
+    fs.readFile("files/highscore.json", "utf8", function(err, data){
+        console.log("read File");
+        if(err) {
+            console.log("Error");
+            res.status(500);
+            return;
+        }
 
-		// Return Highscores
-    console.log("kein Error");
-		res.status(200).json(JSON.parse(data));
-	});
+        // Return Highscores
+        console.log("kein Error");
+        res.status(200).json(JSON.parse(data));
+    });
 });
 
-router.post('/', (req, res) => {
-	// Read Highscores
-	fs.readFile("files/highscore.json", "utf8", (err, data) => {
-		if(err) {
-			res.status(500);
-			return;
-		}
-		let json = JSON.parse(data);
+router.post("/", (req, res) => {
+    // Read Highscores
+    fs.readFile("files/highscore.json", "utf8", (err, data) => {
+        if(err) {
+            res.status(500);
+            return;
+        }
+        let json = JSON.parse(data);
 
-		// Add new Highscore
-		json.highscore.push(
-			{
-				name: req.body.name,
-				points: req.body.points
-			}
-		);
+        // Add new Highscore
+        json.highscore.push(
+            {
+                name: req.body.name,
+                points: req.body.points
+            }
+        );
 
-		// Write new Highscores
-		fs.writeFile('files/highscore.json', JSON.stringify(json), 'utf8', (err) => {
-			if(err) {
-				res.status(500);
-				return;
-			}
-			res.status(201);
-		});
-	});
+        // Write new Highscores
+        fs.writeFile("files/highscore.json", JSON.stringify(json), "utf8", (err) => {
+            if(err) {
+                res.status(500);
+                return;
+            }
+            res.status(201);
+        });
+    });
 
-})
+});
 
 module.exports = router;
